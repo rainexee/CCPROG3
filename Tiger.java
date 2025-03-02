2482e165aa93397f7605e77343a677c8eb294d1a
rain. <157507549+rainexee@users.noreply.github.com>
2025-03-02 21:18:26 +0800

    Tiger.java

--- Tiger.java
+++ Tiger.java
@@ -1,20 +1,14 @@
-class Tiger extends Animal{
-private static final int LEVEL = 5; // if level higher it can take the animal out of play
-private static final int WEAKER = 2;
-    public Tiger() {
-        this.animalName = "Tiger";
-        this.can_swim = true;
-    }
-    public static void main(String[] args) //tester
-    {
-        Tiger tiger = new Tiger();
-        System.out.println("Animal Name: " + tiger.animalName);
-        System.out.println("Can Swim: "+ tiger.can_swim);
-        if(tiger.LEVEL > tiger.WEAKER)
-        {
-            System.out.println("Tiger BEATS WEAK");
+public class Tiger extends Animal {
+    private static final int LEVEL = 5; // if level higher it can take the animal out of play
+        public Tiger(String animalName, boolean can_swim) {
+            super(animalName, can_swim);
         }
+        public Tiger() {
+            super("Tiger", true);
+            if(Tiger.LEVEL > super.getLevel())
+            {
+                System.out.println("Tiger BEATS " + this.getAnimalName());
+            }
+        }
+    
     }
-
-
-}
