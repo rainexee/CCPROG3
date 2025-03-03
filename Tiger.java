9f396b022b1ba8418668df6be923e1f97e1e48d4
rain. <157507549+rainexee@users.noreply.github.com>
2025-03-03 18:15:06 +0800

    Tiger.java
    Dynamic Leveling due to traps now.

--- Tiger.java
+++ Tiger.java
@@ -1,14 +1,18 @@
 public class Tiger extends Animal {
-    private static final int LEVEL = 5; // if level higher it can take the animal out of play
-        public Tiger(String animalName, boolean can_swim) {
-            super(animalName, can_swim);
-        }
-        public Tiger() {
-            super("Tiger", true);
-            if(Tiger.LEVEL > super.getLevel())
-            {
-                System.out.println("Tiger BEATS " + this.getAnimalName());
-            }
-        }
-    
+    private static final int LEVEL = 5;
+
+    public Tiger(String animalName, boolean can_swim) {
+        super(animalName, can_swim);
+        this.strLv = LEVEL; // Initialize strLv with LEVEL
     }
+
+    public Tiger() {
+        super("Tiger", true);
+        this.strLv = LEVEL; // Initialize strLv with LEVEL
+    }
+
+    @Override
+    public int getLevel() {
+        return this.strLv; // Return the dynamic level
+    }
+}
