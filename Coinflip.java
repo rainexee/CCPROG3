b1360666f7198ea742b43d5dd318636727658440
rain. <157507549+rainexee@users.noreply.github.com>
2025-03-07 21:29:42 +0800

    Update Coinflip.java

--- Coinflip.java
+++ Coinflip.java
@@ -8,7 +8,7 @@ public class Coinflip {
     public static boolean determineFirstPlayer(Scanner myObj) {
         // Create a list of numbers from 1 to 8
         List<Integer> animalNumbers = new ArrayList<>();
-        for (int i = 1; i <= 8; i++) {
+        for (int i = 1; i <= 2; i++) {
             animalNumbers.add(i);
         }
 
@@ -16,12 +16,6 @@ public class Coinflip {
         List<Integer> animalStrengths = new ArrayList<>();
         animalStrengths.add(5); // Strength of Elephant
         animalStrengths.add(3); // Strength of Wolf
-        animalStrengths.add(4); // Strength of Leopard
-        animalStrengths.add(1); // Strength of Rat
-        animalStrengths.add(3); // Strength of Cat
-        animalStrengths.add(4); // Strength of Dog
-        animalStrengths.add(6); // Strength of Tiger
-        animalStrengths.add(7); // Strength of Lion
 
         // literally random seed from ccprog1-2
         long seed = System.nanoTime();
@@ -29,14 +23,16 @@ public class Coinflip {
         Collections.shuffle(animalStrengths, new Random(seed));
 
         // Player 1 picks a number, while loop checks if number out of range via negation
-        System.out.println("Player 1, pick a number from 1 to 8:");
+        System.out.println("Player 1, pick a number from 1/2:");
         int playerOneNumber = myObj.nextInt();
-        while (!animalNumbers.contains(playerOneNumber)) {
-            System.out.println("Invalid number. Please pick a number from 1 to 8:");
+        while (!animalNumbers.contains(playerOneNumber)) {         
+            System.out.println("Invalid number. Please pick a number from 1 to 2:");
             playerOneNumber = myObj.nextInt();
         }
         animalNumbers.remove(Integer.valueOf(playerOneNumber));
+        int playerTwoNumber = animalNumbers.get(0);
 
+        /* 
         // Player 2 picks a number, while loop checks if number out of range via negation
         System.out.println("Player 2, pick a number from the remaining numbers:");
         int playerTwoNumber = myObj.nextInt();
@@ -44,7 +40,7 @@ public class Coinflip {
             System.out.println("Invalid number. Please pick a number from the remaining numbers:");
             playerTwoNumber = myObj.nextInt();
         }
-        
+        */
         // -1 because index notation
         int playerOneStrength = animalStrengths.get(playerOneNumber - 1);
         int playerTwoStrength = animalStrengths.get(playerTwoNumber - 1);
