7c06760393b38e0515f0c9718723bf5c28d53c58
rain. <157507549+rainexee@users.noreply.github.com>
2025-03-07 21:35:02 +0800

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,69 +1,25 @@
-package javaprac;
-
-import java.util.Date;
-
 import java.util.Scanner;
-import java.util.ArrayList;
 
 public class Main {
 	public static void main(String[] args) {
 		int winState = 0;
-		int currentPlayer = 1;
+		//int currentPlayer = 1;				
 		
 		//Player player1 = new Player(1);
+
 		Room room1 = new Room(1);
-		
+		System.out.println("Begin Game");
+		Scanner myObj = new Scanner(System.in);
+		room1.getPlayer(1).turnActive = Coinflip.determineFirstPlayer(myObj);
 		room1.getTiles();
 		while(winState == 0) {
-			if(room1.getPlayer(1).turnActive == true) {
+			if(room1.getPlayer(1).turnActive  == true) {
 				room1.getPlayer(1).selectAnimal();
 			}else{
 				room1.getPlayer(2).selectAnimal();
 			}
 		}
 		
-		//room1.tiles[0][2].getTileInfo();
-		
-		//room1.getPlayer(1).moveAnimal();
-		//animal1.owner = player1;
-		/*Room room1 = new Room();
-		room1.getTiles();
-		
-		room1.movePersonRight();
-		Scanner myObj = new Scanner(System.in);
-		String input;
-		
-		do{
-			input = myObj.nextLine();
-			switch(input) {
-				case "w":
-					room1.movePersonUp();
-					input = "y";
-					break;
-				case "a":
-					room1.movePersonLeft();
-					input = "y";
-					break;
-				case "s":
-					room1.movePersonDown();
-					input = "y";
-					break;
-				case "d":
-					System.out.println("Moving right");
-					room1.movePersonRight();
-					input = "y";
-					break;
-				default:
-					input = "X";
-			}
-		}while(input == "y");
-		System.out.println("Program terminated");*/
 	}
 }
 
-/*public class Person{
-	int age;
-	String name;
-	String occupation;
-	int id;
-}*/
